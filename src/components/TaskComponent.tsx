c54ec8ce30e1d5710b1e8268b7356d6349a851b0
devitoDbug <davidOchiengy@gmail.com>
2024-01-02 13:15:23 +0300

    feat: Implement click outside detection for closing edit box

--- src/components/TaskComponent.tsx
+++ src/components/TaskComponent.tsx
@@ -1,4 +1,11 @@
-import { FC, useContext } from "react";
+import {
+  FC,
+  FormEventHandler,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCheck,
@@ -12,6 +19,9 @@ import "../styles/taskComponent.scss";
 
 const TaskComponent: FC<Task> = (task) => {
   const { setTasks } = useContext(TaskContext);
+  const [showEditBox, setShowEditBox] = useState<boolean>(false);
+  const [edits, setEdits] = useState<string>("");
+  const editRef = useRef<HTMLDivElement>(null);
 
   const handleToggleStatus = () => {
     setTasks((prev: Task[]) => {
@@ -21,9 +31,55 @@ const TaskComponent: FC<Task> = (task) => {
     });
   };
 
+  const handleEditTask = () => {
+    setShowEditBox(true);
+  };
+
+  const handleSubmitEditedTask: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    setShowEditBox(false);
+    if (edits.length > 0) {
+      setTasks((prevArry) => {
+        return prevArry.map((element) =>
+          element.id === task.id ? { ...element, name: edits } : element,
+        );
+      });
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutsideEditBox = (event: MouseEvent) => {
+      if (editRef.current && !editRef.current.contains(event.target as Node)) {
+        setShowEditBox(false);
+        setEdits(task.name);
+      }
+    };
+
+    if (showEditBox) {
+      document.addEventListener("click", handleClickOutsideEditBox);
+    } else {
+      document.removeEventListener("click", handleClickOutsideEditBox);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutsideEditBox);
+    };
+  }, [showEditBox, task.name]);
+
   return (
-    <div className="taskContainer">
-      <span className="taskDescription">{task.name}</span>
+    <div className="taskContainer" ref={editRef}>
+      {showEditBox ? (
+        <form className="editTextContainer" onSubmit={handleSubmitEditedTask}>
+          <input
+            className="editTextInputBox"
+            onChange={(e) => setEdits(e.target.value)}
+            placeholder="Enter text..."
+            value={edits}
+          />
+        </form>
+      ) : (
+        <span className="taskDescription">{task.name}</span>
+      )}
       <div className="buttonArea">
         <button onClick={handleToggleStatus}>
           {task.completed ? (
@@ -35,7 +91,7 @@ const TaskComponent: FC<Task> = (task) => {
         <button>
           <FontAwesomeIcon className="icons " icon={faTrash} />
         </button>
-        <button>
+        <button onClick={handleEditTask}>
           <FontAwesomeIcon className="" icon={faEdit} />
         </button>
       </div>
